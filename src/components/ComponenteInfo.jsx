84239aaed5b3284638cc1948526c45aca28589f4
santiago <santysjr956@gmail.com>
2022-05-11 12:06:43 -0300

    add descripcion and titulo

--- src/components/ComponenteInfo.jsx
+++ src/components/ComponenteInfo.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+const noticia = {
+	"titulo":"este es mi titulo",
+	"descripcion":"este es mi descripcion"
+} 
 
 function CoponenteInfo (){
+const {titulo,descripcion} = noticia;
+
 	return(
 
 		<div>
 			<h1>informatorio chaco 2022</h1>
+			<p>{titulo}</p>
+			<p>{descripcion}</p>			
 		</div>
 		)
 }
