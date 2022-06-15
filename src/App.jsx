3c0f7433bfe7388a685ba17954759c16aa4fd1cc
santiago <santysjr956@gmail.com>
2022-06-15 13:21:32 -0300

    add component caerd and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
-import ComponenteInfo from './components/ComponenteInfo'
+import {Route, Routes} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <ComponenteInfo></ComponenteInfo>
-        
-        <p> React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-         
-        </p>
-      </header>
-    </div>
+    
+     <Routes>
+  <Route path="/" element={<HomePage />}/>
+    
+</Routes>
+   
   )
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import CardComponent from "../components/CardComponent"
+import Container from 'react-bootstrap/Container';
+import Header from "../components/Header"
+import Stack from 'react-bootstrap/Stack';
+function HomePage() {
+ 
+
+    return (
+      <>
+        <Header></Header>
+        <Container>
+        <Stack direction="horizontal" gap={3} className="my-md-5">
+        <CardComponent></CardComponent>
+        <CardComponent></CardComponent>
+        <CardComponent></CardComponent>
+    </Stack>
+    </Container>
+
+      
+
+      </>
+    )
+  }
+  
+  export default HomePage
